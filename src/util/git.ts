e253778cc6c2c03d675982a84c22bdabed1e5831
Ian Skelskey <ianskelskey@gmail.com>
2024-11-16 20:18:18 -0500

    Add coverage entry to .gitignore and clean coverage files
    - Added `coverage/` to `.gitignore` to exclude coverage reports from version control.
    - Removed existing coverage report files and directories to clean up the repository, including `clover.xml`, `coverage-final.json`, and the `lcov-report` directory.
    
    Release-Note: exclude coverage reports from source control by adding `coverage/` to `.gitignore`.
    
    Signed-off-by: Ian Skelskey <ianskelskey@gmail.com>

--- src/util/git.ts
+++ src/util/git.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 
-enum GitFileStatus {
+export enum GitFileStatus {
     'A' = 'Added',
     'M' = 'Modified',
     'D' = 'Deleted',
